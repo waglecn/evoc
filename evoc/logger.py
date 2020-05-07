4b54be9e2a1eba1146ec6307b8e1964b8ae59b58
Nick Waglechner <nwaglechner@gmail.com>
2020-05-06 20:29:38 -0400

    Updates logger with name

--- evoc/logger.py
+++ evoc/logger.py
@@ -4,5 +4,5 @@ logging.basicConfig(
     format='%(asctime)-15s - %(module)s - %(levelname)s - %(message)s',
     datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO
 )
-logger = logging.getLogger()
+logger = logging.getLogger('evoc')
 logger.setLevel(logging.INFO)
