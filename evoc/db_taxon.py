55f7cf207575e2ff312d1b378b6b00f968765054
Nick Waglechner <nwaglechner@gmail.com>
2020-05-07 15:45:35 -0400

    Adds better exception handling to db_taxon, and associated tests.

--- evoc/db_taxon.py
+++ evoc/db_taxon.py
@@ -1,3 +1,10 @@
+from collections import namedtuple
+import sqlite3
+from evoc.logger import logger
+
+TaxonRow = namedtuple('TaxonRow', 'taxon_id, type_id, NCBI_tax_id')
+
+
 def taxon_init():
     """Taxon table init string"""
 
@@ -9,7 +16,6 @@ def taxon_init():
             FOREIGN KEY (type_id) REFERENCES type(type_id)
         )
     """
-
     return TAXON_SQL
 
 
@@ -19,9 +25,16 @@ def add_taxon(
     type_id=None
 ):
 
+    try:
+        assert connection is not None
+    except AssertionError:
+        logger.exception('Invalid connection supplied')
+        raise SystemExit('Exiting.')
+
     fields = []
     values = []
     items = []
+
     try:
         if NCBI_tax_id is not None:
             fields.append('NCBI_tax_id')
@@ -31,27 +44,42 @@ def add_taxon(
             fields.append('type_id')
             int(type_id)
             items.append(type_id)
+    except ValueError:
+        logger.exception('Invalid paramters supplied')
+        raise SystemExit('Exiting.')
 
-        assert(len(fields) == len(items))
+    assert(len(fields) == len(items))
+
+    try:
         assert len(fields) > 0
+    except AssertionError:
+        logger.exception('Incorrect number of paramters supplied')
+        raise SystemExit('Exiting.')
 
-        for x in range(len(fields)):
-            values.append('?')
+    for x in range(len(fields)):
+        values.append('?')
 
-        fields = ', '.join(fields)
-        values = ', '.join(values)
-        items = tuple(items)
+    fields = ', '.join(fields)
+    values = ', '.join(values)
+    items = tuple(items)
 
-        cmd = """
-            INSERT INTO taxon (""" + fields + """)
-            VALUES (""" + values + """)
-        """
+    cmd = """
+        INSERT INTO taxon (""" + fields + """)
+        VALUES (""" + values + """)
+    """
+
+    try:
         cur = connection.cursor()
         cur.execute(cmd, items)
-        return True
+        return TaxonRow(*connection.execute(
+            'SELECT taxon_id, type_id, NCBI_tax_id FROM taxon '
+            'WHERE taxon_id = ?', (cur.lastrowid,)
+        ).fetchone())
+    except sqlite3.IntegrityError:
+        logger.exception('Supplied invalid taxon_id')
+        raise SystemExit('Exiting.')
     except Exception as e:
         print('Caught: {0}'.format(str(e)))
-        return False
 
 
 def check_taxon(
@@ -61,10 +89,16 @@ def check_taxon(
     type_id=None
 ):
     """ Returns taxon list, or False"""
-    cur = connection.cursor()
+
+    try:
+        assert connection is not None
+    except AssertionError:
+        logger.exception('Invalid connection')
+        raise SystemExit('Exiting.')
 
     fields = []
     values = []
+
     try:
         if taxon_id is not None:
             fields.append('taxon_id = ?')
@@ -78,30 +112,32 @@ def check_taxon(
             fields.append('type_id = ?')
             int(type_id)
             values.append(type_id)
+    except ValueError:
+        logger.exception('Invalid parameter supplied')
+        raise SystemExit('Exiting.')
 
-        assert (len(fields) == len(values))
-        if len(fields) == 0:
-            cmd = """
-                SELECT
-                    taxon_id,
-                    type_id,
-                    NCBI_tax_id
-                FROM taxon
-            """
-        else:
-            fields = ' AND '.join(fields)
-            values = tuple(values)
+    assert (len(fields) == len(values))
+    if len(fields) == 0:
+        cmd = """
+            SELECT
+                taxon_id,
+                type_id,
+                NCBI_tax_id
+            FROM taxon
+        """
+    else:
+        fields = ' AND '.join(fields)
+        values = tuple(values)
 
-            cmd = """
-                SELECT
-                    taxon_id,
-                    type_id,
-                    NCBI_tax_id
-                FROM taxon
-                WHERE """ + fields
-
-        results = cur.execute(cmd, values).fetchall()
-        return results
+        cmd = """
+            SELECT
+                taxon_id,
+                type_id,
+                NCBI_tax_id
+            FROM taxon
+            WHERE """ + fields
+    try:
+        results = connection.execute(cmd, values).fetchall()
+        return [TaxonRow(*r) for r in results]
     except Exception as e:
         print('Caught: {0}'.format(str(e)))
-        return False
--- evoc/test/test_db_taxon.py
+++ evoc/test/test_db_taxon.py
@@ -1,26 +1,33 @@
+import os
 from evoc import db
 from nose.tools import assert_equal
-from nose.tools import assert_not_equal
+from nose.tools import assert_raises
+from evoc.logger import logger
+
+logger.setLevel(5)
 
 
 class Test_db_taxon:
     def __init__(self):
         self.NCBI_tax_id = 2
-        self.name = 'Example'
         self.taxon_id = 1
         self.type_id = 1
 
     @classmethod
     def setup_class(cls):
-        cls.connection = db.init_db('./testdb.sqlite3')
+        cls.dbname = './testdb.sqlite3'
+        cls.connection = db.init_db(cls.dbname)
 
     @classmethod
     def teardown_class(cls):
         cmd = "DROP TABLE IF EXISTS taxon"
-        cur = cls.connection.cursor()
-        cur.execute(cmd)
+        cls.connection.execute(cmd)
         cls.connection.commit()
         cls.connection.close()
+        try:
+            os.remove(cls.dbname)
+        except OSError:
+            pass
 
     def test_01_add_taxon_1(self):
         """adding a taxon with all information"""
@@ -29,129 +36,107 @@ class Test_db_taxon:
         NCBI_tax_id = self.NCBI_tax_id
         type_id = self.type_id
 
-        assert_equal(
-            db.add_taxon(
-                connection,
-                NCBI_tax_id=NCBI_tax_id,
-                type_id=type_id
-            ), True
+        result = db.add_taxon(
+            connection,
+            NCBI_tax_id=NCBI_tax_id,
+            type_id=type_id,
         )
+        assert_equal(True, isinstance(result, db.TaxonRow))
 
     def test_02_add_taxon_2(self):
         """adding another taxon with all information"""
 
         connection = self.connection
         NCBI_tax_id = 2
-        type_id = 1
+        type_id = 2
 
-        assert_equal(
-            db.add_taxon(
-                connection,
-                NCBI_tax_id=NCBI_tax_id,
-                type_id=type_id
-            ), True
+        db.add_taxon(
+            connection,
+            NCBI_tax_id=NCBI_tax_id,
+            type_id=type_id
         )
 
     def test_03_add_taxon_null(self):
-        """adding a taxon with no information"""
+        """Try adding a taxon with no information"""
         connection = self.connection
-        assert_equal(db.add_taxon(connection), False)
+        assert_raises(SystemExit, db.add_taxon, connection)
 
     def test_04_check_taxon_by_NCBI_id(self):
         """Checking via taxon.NCBI_tax_id"""
         connection = self.connection
         NCBI_tax_id = self.NCBI_tax_id
-        results = db.check_taxon(connection, NCBI_tax_id=NCBI_tax_id)
-        assert_equal(len(results), 2)
-        item = (
-            self.taxon_id,
-            self.type_id,
-            self.NCBI_tax_id
-        )
-        assert_equal(results[0], item)
+        result = db.check_taxon(connection, NCBI_tax_id=NCBI_tax_id)
+        assert_equal(len(result), 2)
 
     def test_05_check_taxon_by_taxon_id(self):
         """Checking via taxon.taxon_id"""
         connection = self.connection
         taxon_id = self.taxon_id
-        results = db.check_taxon(connection, taxon_id=taxon_id)
-        assert_not_equal(results, False)
-        assert_equal(len(results), 1)
-        item = (
-            self.taxon_id,
-            self.type_id,
-            self.NCBI_tax_id
-        )
-        assert_equal(results[0], item)
+        result = db.check_taxon(connection, taxon_id=taxon_id)
+        assert_equal(len(result), 1)
 
-    def test_06_check_taxon_by_mixed_1(self):
-        """Checking via mixed information (NCBI_tax_id, description)"""
-        connection = self.connection
-        NCBI_tax_id = self.NCBI_tax_id
-        results = db.check_taxon(
-            connection,
-            NCBI_tax_id=NCBI_tax_id,
-        )
-        item = (
-            self.taxon_id,
-            self.type_id,
-            self.NCBI_tax_id
-        )
-        assert_equal(results[0], item)
-
-    def test_07_check_multiple_taxa(self):
+    def test_06_check_multiple_taxa(self):
         """Checking via NCBI_tax_id (2:Bacteria) for 2 rows"""
         connection = self.connection
         NCBI_tax_id = self.NCBI_tax_id
-        results = db.check_taxon(
+        result = db.check_taxon(
             connection,
             NCBI_tax_id=NCBI_tax_id
         )
-        item = (
-            self.taxon_id,
-            self.type_id,
-            self.NCBI_tax_id
-        )
-        item2 = (2, 1, 2)
-        assert_equal(results[0], item)
-        assert_equal(results[1], item2)
+        assert_equal(len(result), 2)
 
-    def test_08_add_invalid_taxon(self):
-        """Try to add a taxon with invalid data"""
+    def test_07_add_invalid_taxon(self):
+        """Try add a taxon with invalid data"""
         connection = self.connection
-        result = db.add_taxon(
+        assert_raises(
+            SystemExit,
+            db.add_taxon,
             connection,
             NCBI_tax_id='X',
             type_id=1
         )
-        assert_equal(result, False)
 
-        result = db.add_taxon(
+        assert_raises(
+            SystemExit,
+            db.add_taxon,
             connection,
             NCBI_tax_id=1,
             type_id='X'
         )
-        assert_equal(result, False)
 
-        result = db.add_taxon(
+        assert_raises(
+            SystemExit,
+            db.add_taxon,
             None,
             NCBI_tax_id=self.NCBI_tax_id,
             type_id=self.type_id
         )
 
-    def test_09_check_invalid_params(self):
+    def test_08_check_invalid_params(self):
         """Check using invalid paramters"""
         connection = self.connection
-        result = db.check_taxon(connection, taxon_id='X')
-        assert_equal(result, False)
+        assert_raises(
+            SystemExit,
+            db.check_taxon,
+            connection,
+            taxon_id='X'
+        )
 
-        result = db.check_taxon(connection, NCBI_tax_id='X')
-        assert_equal(result, False)
+        assert_raises(
+            SystemExit,
+            db.check_taxon,
+            connection,
+            NCBI_tax_id='X'
+        )
 
-        result = db.check_taxon(connection, type_id='X')
-        assert_equal(result, False)
+        assert_raises(
+            SystemExit,
+            db.check_taxon,
+            connection,
+            type_id='X'
+        )
 
-    def test_10_check_empty_result(self):
+    def test_09_check_empty_result(self):
         """Check for correctly returned empty result"""
         connection = self.connection
         result = db.check_taxon(connection, taxon_id=9999999)
@@ -163,8 +148,13 @@ class Test_db_taxon:
         result = db.check_taxon(connection, type_id=99999999)
         assert_equal(result, [])
 
-    def test_11_check_for_all_result(self):
-        """Check for the result without paramters"""
+    def test_10_add_nonexistant_taxon_type(self):
+        """Try to add a taxon with a non-existant type_id"""
         connection = self.connection
-        result = db.check_taxon(connection)
-        assert_equal(len(result) > 1, True)
+        assert_raises(
+            SystemExit,
+            db.add_taxon,
+            connection,
+            type_id=99999,
+            NCBI_tax_id=2
+        )
