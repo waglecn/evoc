98cb36cf306fd3b484f5305e313b7d289ea638c9
Nick Waglechner <nwaglechner@gmail.com>
2020-05-08 09:21:56 -0400

    Adds enhanced exception checking in db cluster_gene.
    Adds logger and overrides default logging on tests. Returns namedtuples, and the
    added item on successful_cluster_gene. Checks for bad conncetions, and enforces
    foreign key constraings. Lacks proper docstrings.

--- evoc/db_cluster_gene.py
+++ evoc/db_cluster_gene.py
@@ -1,7 +1,16 @@
+import sqlite3
+from collections import namedtuple
+from evoc.logger import logger
+
+ClusterGeneRow = namedtuple(
+    'ClusterGeneRow', 'cluster_gene_id, cluster_id, gene_id'
+)
+
+
 def cluster_gene_init():
     """cluster_gene table init string"""
 
-    cluster_gene = """
+    CLUSTER_GENE_SQL = """
         CREATE TABLE IF NOT EXISTS cluster_gene(
             cluster_gene_id INTEGER UNIQUE PRIMARY KEY NOT NULL,
             cluster_id INTEGER NOT NULL,
@@ -10,25 +19,34 @@ def cluster_gene_init():
             FOREIGN KEY (gene_id) REFERENCES gene(gene_id)
         )
     """
-    return cluster_gene
+    return CLUSTER_GENE_SQL
 
 
 def add_cluster_gene(connection, cluster_id=None, gene_id=None):
     """Add a single entry to the cluster_gene table"""
+    try:
+        assert connection is not None
+        cur = connection.cursor()
+    except (AssertionError, AttributeError):
+        logger.exception('Invalid connection supplied')
+        raise SystemExit('Exiting.')
 
-    cur = connection.cursor()
     insert = []
     values = []
     where = []
 
-    if cluster_id is not None:
-        insert.append('cluster_id')
-        values.append('?')
-        where.append(cluster_id)
-    if gene_id is not None:
-        insert.append('gene_id')
-        values.append('?')
-        where.append(gene_id)
+    try:
+        if cluster_id is not None:
+            insert.append('cluster_id')
+            values.append('?')
+            where.append(int(cluster_id))
+        if gene_id is not None:
+            insert.append('gene_id')
+            values.append('?')
+            where.append(int(gene_id))
+    except ValueError:
+        logger.exception('Invalid parameter supplied')
+        raise SystemExit('Exiting.')
 
     assert len(values) == len(where) == len(insert)
     if len(values) > 0:
@@ -36,20 +54,21 @@ def add_cluster_gene(connection, cluster_id=None, gene_id=None):
         values = ', '.join(values)
         where = tuple(where)
 
-        cmd = """
-            INSERT INTO cluster_gene (""" + insert + """) VALUES(""" + values \
-            + """)
-        """
-        try:
-            for i in where:
-                int(i)
-
-            cur.execute(cmd, where)
-            return True
-        except Exception as e:
-            print(e)
-
-    return False
+    try:
+        cmd = "INSERT INTO cluster_gene ({}) VALUES({})".format(
+            insert, values
+        )
+        cur.execute(cmd, where)
+        return ClusterGeneRow(*cur.execute(
+            "SELECT cluster_gene_id, cluster_id, gene_id FROM cluster_gene "
+            "WHERE cluster_gene_id = ?", (cur.lastrowid,)
+        ).fetchone())
+    except sqlite3.IntegrityError:
+        logger.exception('Invalid id paramter supplied')
+        raise SystemExit('Exiting.')
+    except Exception as e:
+        logger.exception('Error trying to add cluster_gene: {}'.format(e))
+        raise SystemExit('Exiting.')
 
 
 def check_cluster_gene(
@@ -61,51 +80,45 @@ def check_cluster_gene(
     """Check for a single entry in the cluster_gene table by id, gene_id,
         cluster_id. If none provided, will return all entries in the table"""
 
+    try:
+        assert connection is not None
+    except AssertionError:
+        logger.exception('Invalind connection supplied')
+        raise SystemExit('Exiting.')
+
     values = []
     where = []
 
-    if cluster_gene_id is not None:
-        where.append('cluster_gene_id = ?')
-        values.append(cluster_gene_id)
-    if gene_id is not None:
-        where.append('gene_id = ?')
-        values.append(gene_id)
-    if cluster_id is not None:
-        where.append('cluster_id = ?')
-        values.append(cluster_id)
+    try:
+        if cluster_gene_id is not None:
+            where.append('cluster_gene_id = ?')
+            values.append(int(cluster_gene_id))
+        if gene_id is not None:
+            where.append('gene_id = ?')
+            values.append(int(gene_id))
+        if cluster_id is not None:
+            where.append('cluster_id = ?')
+            values.append(int(cluster_id))
+    except ValueError:
+        logger.exception('Invalid id parameter supplied')
+        raise SystemExit('Exiting.')
 
     assert(len(values) == len(where))
 
+    cmd = """ SELECT cluster_gene_id, cluster_id, gene_id
+        FROM cluster_gene"""
     try:
-        cur = connection.cursor()
-        if len(values) == 0:
-            cmd = """
-                SELECT
-                    cluster_gene_id,
-                    cluster_id,
-                    gene_id
-                FROM cluster_gene
-            """
-            results = cur.execute(cmd).fetchall()
-            return results
-        else:
-            for i in values:
-                int(i)
+        if len(values) > 0:
             where = ' AND '.join(where)
             values = tuple(values)
-            cmd = """
-                SELECT
-                    cluster_gene_id,
-                    cluster_id,
-                    gene_id
-                FROM cluster_gene
-            WHERE """ + where
-            results = cur.execute(cmd, values).fetchall()
-            if len(results) > 0:
-                return results
-            else:
-                return []
+            cmd += " WHERE " + where
+            result = connection.execute(cmd, values)
+        else:
+            result = connection.execute(cmd)
+        return [ClusterGeneRow(*r) for r in result]
+    except AttributeError:
+        logger.exception('Invalid connection supplied')
+        raise SystemExit('Exiting.')
     except Exception as e:
-        print('Caught: {0}'.format(e))
-
-    return False
+        logger.exception('Caught: {0}'.format(e))
+        raise SystemExit('Exiting.')
--- evoc/test/test_db_cluster_gene.py
+++ evoc/test/test_db_cluster_gene.py
@@ -1,5 +1,9 @@
 from evoc import db
+from evoc.logger import logger
 from nose.tools import assert_equal
+from nose.tools import assert_raises
+
+logger.setLevel(5)
 
 
 class Test_db_cluster_gene:
@@ -10,88 +14,164 @@ class Test_db_cluster_gene:
 
     @classmethod
     def setup_class(cls):
-        cls.connection = db.init_db('./testdb.sqlite3')
+        cls.connection = db.init_db(':memory:')
+        cls.test_taxon = db.add_taxon(
+            cls.connection, type_id=1, NCBI_tax_id=2
+        )
+        cls.test_gb = db.add_gb(
+            cls.connection, cls.test_taxon.taxon_id, prefix='test',
+            gb="", loc='/dev/null'
+        )
+        cls.test_cluster = db.add_cluster(
+            cls.connection, type_id=1
+        )
+        cls.test_gene_1 = db.add_gene(
+            cls.connection, uniquename='test1', gb_id=cls.test_gb.gb_id,
+            type_id=1, pro_id='1', nuc_id='1',
+            location=db.encode_gene_location([1, 1, 5]),
+            seq='AAA', start=1, end=3
+        )
+        cls.test_gene_2 = db.add_gene(
+            cls.connection, uniquename='test2', gb_id=cls.test_gb.gb_id,
+            type_id=1, pro_id='2', nuc_id='2',
+            location=db.encode_gene_location([1, 1, 5]),
+            seq='CCC', start=1, end=3
+        )
+        cls.test_gene_3 = db.add_gene(
+            cls.connection, uniquename='test3', gb_id=cls.test_gb.gb_id,
+            type_id=1, pro_id='3', nuc_id='3',
+            location=db.encode_gene_location([1, 1, 5]),
+            seq='DDD', start=1, end=3
+        )
 
     @classmethod
     def teardown_class(cls):
-        cmd = "DROP TABLE IF EXISTS cluster_gene"
-        cur = cls.connection.cursor()
-        cur.execute(cmd)
-        cls.connection.commit()
-        cls.connection.close()
+        pass
 
-    def test_01_db_add_cluster_gene(self):
+    def test_01_db_add_cluster_genes(self):
         """Add a cluster_gene entry"""
         connection = self.connection
-        cluster_id = self.cluster_id
-        gene_id = self.gene_id
-        result = db.add_cluster_gene(
+        cluster_id = self.test_cluster.cluster_id
+        result1 = db.add_cluster_gene(
             connection,
             cluster_id=cluster_id,
-            gene_id=gene_id
+            gene_id=self.test_gene_1.gene_id
         )
-        assert_equal(result, True)
+        result2 = db.add_cluster_gene(
+            connection,
+            cluster_id=cluster_id,
+            gene_id=self.test_gene_2.gene_id
+        )
+        result3 = db.add_cluster_gene(
+            connection,
+            cluster_id=cluster_id,
+            gene_id=self.test_gene_3.gene_id
+        )
+        assert_equal(True, isinstance(result1, db.ClusterGeneRow))
+        assert_equal(True, isinstance(result2, db.ClusterGeneRow))
+        assert_equal(True, isinstance(result3, db.ClusterGeneRow))
 
     def test_02_db_check_cluster_gene_by_cluster_gene_id(self):
         """Check a cluster by cluster_id"""
         connection = self.connection
-        cluster_gene_id = self.cluster_gene_id
-        cluster_id = self.cluster_id
-        gene_id = self.gene_id
         result = db.check_cluster_gene(
             connection,
-            cluster_gene_id=cluster_gene_id
+            cluster_gene_id=1
         )
-        item = (cluster_gene_id, cluster_id, gene_id)
-        assert_equal(result[0], item)
+        assert_equal(True, isinstance(result[0], db.ClusterGeneRow))
 
     def test_03_db_check_cluster_gene_without_selection(self):
         """Check all clusters"""
         connection = self.connection
         result = db.check_cluster_gene(connection)
-        assert_equal(len(result), 1)
+        assert_equal(len(result), 3)
 
     def test_04_db_add_invalid_cluster_gene(self):
         """add an invalid cluster_gene entry"""
         connection = self.connection
-        result = db.add_cluster_gene(
-            connection,
+        assert_raises(
+            SystemExit,
+            db.add_cluster_gene, connection,
             gene_id=1,
             cluster_id='X'
         )
-        assert_equal(result, False)
-
-        result = db.add_cluster_gene(
-            connection,
+        assert_raises(
+            SystemExit,
+            db.add_cluster_gene, connection,
+            gene_id=1,
+            cluster_id=None
+        )
+        assert_raises(
+            SystemExit,
+            db.add_cluster_gene, connection,
             gene_id='X',
             cluster_id=1
         )
-        assert_equal(result, False)
+        assert_raises(
+            SystemExit,
+            db.add_cluster_gene, connection,
+            gene_id=None,
+            cluster_id=1
+        )
 
     def test_05_db_check_cluster_gene_by_cluster_id(self):
-        """Check for a cluster by cluster_id"""
+        """Check with cluster_id"""
         connection = self.connection
-        cluster_gene_id = self.cluster_gene_id
         cluster_id = self.cluster_id
-        gene_id = self.gene_id
+        result = db.check_cluster_gene(connection, cluster_id=cluster_id)
+        assert_equal(len(result), 3)
 
-        item = (cluster_gene_id, cluster_id, gene_id)
-
-        result = db.check_cluster_gene(connection, cluster_id=1)
-        assert_equal(result[0], item)
-
-        result = db.check_cluster_gene(connection, gene_id=1)
-        assert_equal(result[0], item)
-
-    def test_05_db_check_invalid(self):
+    def test_06_db_check_invalid(self):
         """Check for a cluster using invalid parameters"""
         connection = self.connection
         result = db.check_cluster_gene(connection, gene_id=300)
         assert_equal(result, [])
 
-        result = db.check_cluster_gene(connection, gene_id='X')
-        assert_equal(result, False)
+        result = db.check_cluster_gene(connection, cluster_id=300)
+        assert_equal(result, [])
 
-        connection = None
-        result = db.check_cluster_gene(connection)
-        assert_equal(result, False)
+    def test_07_check_invalid_connection(self):
+        """Try supplying invalid connection"""
+        assert_raises(
+            SystemExit,
+            db.add_cluster_gene,
+            None,
+            gene_id=3, cluster_id=1
+        )
+        assert_raises(
+            SystemExit,
+            db.add_cluster_gene,
+            'X',
+            gene_id=3, cluster_id=1
+        )
+
+        assert_raises(
+            SystemExit,
+            db.check_cluster_gene,
+            None,
+            cluster_id=1
+        )
+        assert_raises(
+            SystemExit,
+            db.check_cluster_gene,
+            'X',
+            cluster_id=1
+        )
+
+    def test_08_add_nonexistant_type_cluster(self):
+        """Try violoating foreign key constraint"""
+        connection = self.connection
+        assert_raises(
+            SystemExit,
+            db.add_cluster_gene,
+            connection,
+            gene_id=300,
+            cluster_id=1
+        )
+        assert_raises(
+            SystemExit,
+            db.add_cluster_gene,
+            connection,
+            gene_id=1,
+            cluster_id=300
+        )
